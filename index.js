8fa1934ba47300e5c5abcd41190b3e93e1ad1e04
mohamed muflahi <muflahimohamed88@gmail.com>
2022-01-31 13:44:08 -0500

    commit tree 12b890eab1d76b918bd2d4c28bcd7700dfb9e844

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+// Code your solution in this file
+function distanceFromHqInBlocks(x){
+    return (Math.abs(x-42)); 
+}
+function distanceFromHqInFeet(x){
+    return distanceFromHqInBlocks(x) * 264;
+}
+function distanceTravelledInFeet(x,y){
+    
+    return (Math.abs(x-y))*264;
+}
+function calculatesFarePrice(x,y){
+
+    let f;
+    let z = distanceTravelledInFeet(x,y);
+    if(z <= 400){
+        return 0;
+    }else if(z <= 2000 && z > 400){
+        f = (z-400)*0.02;
+        return f;
+    }else if(z > 2000 && z<=2500){
+        return 25;
+    }
+    else if(z > 2500){
+        return 'cannot travel that far';
+    }
+}
